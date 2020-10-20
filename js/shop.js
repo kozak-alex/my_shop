807e1850ced19ceb6a7fe8863f192f22cd426052
kozak-alex <kozachenko2578@gmail.com>
2020-10-21 00:18:28 +0300

    home

--- js/shop.js
+++ js/shop.js
@@ -6,7 +6,10 @@ $(() => {
     $('#foto_2').css('opacity', 0.4);
     $('#foto_3').css('opacity', 0.4);
 
-    let bedding_set = {
+    let set = 1;
+
+
+    let bedding_set1 = {
         name: "st033",
         price_onehalf: 1999,
         price_two: 2199,
@@ -15,6 +18,47 @@ $(() => {
     };
 
 
+    let set_st033 = {
+        name: "ST033",
+        size_1_onehalf: '160&times;225',
+        size_2_onehalf: '155&times;215',
+        size_1_two: '180&times;225',
+        size_2_two: '175&times;215',
+        size_1_euro: '225&times;240',
+        size_2_euro: '200&times;220',
+        size_1_family: '225&times;240',
+        size_2_family: '2&times;155&times;215',
+        price_onehalf: 1999,
+        price_two: 2191,
+        price_euro: 2391,
+        price_family: 2991
+    };
+
+    let set_st034 = {
+        name: "ST034",
+        size_1_onehalf: '160&times;225',
+        size_2_onehalf: '155&times;215',
+        size_1_two: '180&times;225',
+        size_2_two: '175&times;215',
+        size_1_euro: '225&times;240',
+        size_2_euro: '200&times;220',
+        size_1_family: '225&times;240',
+        size_2_family: '2&times;155&times;215',
+        price_onehalf: 1991,
+        price_two: 2191,
+        price_euro: 2391,
+        price_family: 2991
+    };
+
+    let bedding_set = [
+        set_st033,
+        set_st034
+    ];
+
+    //    alert(product[1].name);
+
+
+    $('#set_name').text(bedding_set[set].name);
     /**
      * 
      * 
@@ -38,25 +82,25 @@ $(() => {
         const value = $('input[name="option"]:checked').val();
 
         if (value === "1") {
-            $('#price').text('1999 грн');
-            $('#set_size_1').html('Простирадло 160&times;225.');
-            $('#set_size_2').html('Півковдра 155&times;215');
-            alert(bedding_set.price_onehalf);
+            $('#price').text(bedding_set[set].price_onehalf);
+            $('#set_size_1').html(bedding_set[set].size_1_onehalf);
+            $('#set_size_2').html(bedding_set[set].size_2_onehalf);
+            //           alert(bedding_set.price_onehalf);
         } else if (value === "2") {
-            $('#price').text('2199 грн');
-            $('#set_size_1').html('Простирадло 180&times;225.');
-            $('#set_size_2').html('Півковдра 175&times;215');
-            alert(bedding_set.price_two);
+            $('#price').text(bedding_set[set].price_two);
+            $('#set_size_1').html(bedding_set[set].size_1_two);
+            $('#set_size_2').html(bedding_set[set].size_2_two);
+            //           alert(bedding_set.price_two);
         } else if (value === "3") {
-            $('#price').text('2399 грн');
-            $('#set_size_1').html('Простирадло 225&times;240.');
-            $('#set_size_2').html('Півковдра 200&times;220');
-            alert(bedding_set.price_euro);
+            $('#price').text(bedding_set[set].price_euro);
+            $('#set_size_1').html(bedding_set[set].size_1_euro);
+            $('#set_size_2').html(bedding_set[set].size_2_euro);
+            //           alert(bedding_set.price_euro);
         } else {
-            $('#price').text('2999 грн');
-            $('#set_size_1').html('Простирадло 225&times;240.');
-            $('#set_size_2').html('Півковдра 2&times;155&times;215');
-            alert(bedding_set.price_family);
+            $('#price').text(bedding_set[set].price_family);
+            $('#set_size_1').html(bedding_set[set].size_1_family);
+            $('#set_size_2').html(bedding_set[set].size_2_family);
+            //           alert(bedding_set.price_family);
         }
     });
 
@@ -79,6 +123,11 @@ $(() => {
         $('#foto_2').css('opacity', 0.4);
         $('#foto_3').css('opacity', 1);
         $('#foto_big').attr('src', 'images/st033_03.jpg');
+    });
+
+    $('.submenu > li').on('click', () => {
+        //       alert($('.submenu > li').text());
+        alert($('.submenu > li').index());
     });
 
     function radio() {

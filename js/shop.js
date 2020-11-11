0c81ae259e0b63cc2ac8deb71bd7e66b5fbf459c
Kozachenko_Work <kozachenko2578@gmail.com>
2020-11-11 12:23:11 +0200

    11_11

--- js/shop.js
+++ js/shop.js
@@ -7,7 +7,8 @@ $(() => {
     $('#foto_3').css('opacity', 0.4);
 
     let set = 0;
-    let typeBeddingSet = 0;
+    //   let typeBeddingSet = 0;
+    let typeBeddingSet = 'price_onehalf';
 
     /*
         let bedding_set1 = {
@@ -19,12 +20,40 @@ $(() => {
         };
     */
 
+    /*   
+   let set_st033 = {
+    name: "ST033",
+    color_set: 'Колір простирадла - ніжний персик.<br> Півковдри та наволочки - синій.',
+    size_1: ['160&times;225', '180&times;225', '225&times;240', '225&times;240'],
+    size_2: ['155&times;215', '175&times;215', '200&times;220', '2&times;155&times;215'],
+    price_set: [1999, 2199, 2399, 2999],
+    foto_01: 'images/st033_01.jpg',
+    foto_02: 'images/st033_02.jpg',
+    foto_03: 'images/st033_03.jpg'
+};
+*/
+
     let set_st033 = {
         name: "ST033",
         color_set: 'Колір простирадла - ніжний персик.<br> Півковдри та наволочки - синій.',
-        size_1: ['160&times;225', '180&times;225', '225&times;240', '225&times;240'],
-        size_2: ['155&times;215', '175&times;215', '200&times;220', '2&times;155&times;215'],
-        price_set: [1999, 2199, 2399, 2999],
+        size_1: {
+            'price_onehalf': '160&times;225',
+            'price_two': '180&times;225',
+            'price_euro': '225&times;240',
+            'price_family': '225&times;240'
+        },
+        size_2: {
+            'price_onehalf': '155&times;215',
+            'price_two': '175&times;215',
+            'price_euro': '200&times;220',
+            'price_family': '2&times;155&times;215'
+        },
+        price_set: {
+            'price_onehalf': 1999,
+            'price_two': 2199,
+            'price_euro': 2399,
+            'price_family': 2999
+        },
         foto_01: 'images/st033_01.jpg',
         foto_02: 'images/st033_02.jpg',
         foto_03: 'images/st033_03.jpg'
@@ -33,9 +62,24 @@ $(() => {
     let set_se016 = {
         name: "SE016",
         color_set: 'Колір олівковий',
-        size_1: ['160&times;225', '180&times;225', '225&times;240', '225&times;240'],
-        size_2: ['155&times;215', '175&times;215', '200&times;220', '2&times;155&times;215'],
-        price_set: [2499, 2799, 2999, 3499],
+        size_1: {
+            'price_onehalf': '160&times;225',
+            'price_two': '180&times;225',
+            'price_euro': '225&times;240',
+            'price_family': '225&times;240'
+        },
+        size_2: {
+            'price_onehalf': '155&times;215',
+            'price_two': '175&times;215',
+            'price_euro': '200&times;220',
+            'price_family': '2&times;155&times;215'
+        },
+        price_set: {
+            'price_onehalf': 2499,
+            'price_two': 2799,
+            'price_euro': 2999,
+            'price_family': 3499
+        },
         foto_01: 'images/se016_01.jpg',
         foto_02: 'images/se016_02.jpg',
         foto_03: 'images/se016_03.jpg'
@@ -44,9 +88,24 @@ $(() => {
     let set_pf066 = {
         name: "PF066",
         color_set: 'Колір простирадла та наволочки - світло-сірий.<br>Півковдри - світло-сірий, сірий зигзаг',
-        size_1: ['160&times;225', '180&times;225', '225&times;240', '225&times;240'],
-        size_2: ['155&times;215', '175&times;215', '200&times;220', '2&times;155&times;215'],
-        price_set: [1599, 1775, 1850, 2150],
+        size_1: {
+            'price_onehalf': '160&times;225',
+            'price_two': '180&times;225',
+            'price_euro': '225&times;240',
+            'price_family': '225&times;240'
+        },
+        size_2: {
+            'price_onehalf': '155&times;215',
+            'price_two': '175&times;215',
+            'price_euro': '200&times;220',
+            'price_family': '2&times;155&times;215'
+        },
+        price_set: {
+            'price_onehalf': 1599,
+            'price_two': 1775,
+            'price_euro': 1850,
+            'price_family': 2150
+        },
         foto_01: 'images/pf066_01.jpg',
         foto_02: 'images/pf066_02.jpg',
         foto_03: null
@@ -62,23 +121,8 @@ $(() => {
 
 
     $('#set_name').text(bedding_set[set].name);
-    /**
-                 array.push({
-                    date: element.applicable_date,
-                    temperature: element.the_temp,
-                    maxTemperature: element.max_temp,
-                    minTemperature: element.min_temp,
-                    picture: `https://www.metaweather.com/static/img/weather/png/64/${element.weather_state_abbr}.png`
-                })
-     */
+
     /*************************************************************************************/
-    $(check_opt).on('change', () => {
-        const value = $('input[name="option"]:checked').val();
-        const typeBeddingSet = Number(value) - 1;
-        $('#price').text(bedding_set[set].price_set[typeBeddingSet]);
-        $('#set_size_1').html(bedding_set[set].size_1[typeBeddingSet]);
-        $('#set_size_2').html(bedding_set[set].size_2[typeBeddingSet]);
-    });
 
     $('#foto_1').on('click', () => {
         $('#foto_1').css('opacity', 1);
@@ -86,6 +130,23 @@ $(() => {
         $('#foto_3').css('opacity', 0.4);
         $('#foto_big').attr('src', bedding_set[set].foto_01);
     });
+    /*
+        $(check_opt).on('change', () => {
+            const value = $('input[name="option"]:checked').val();
+            const typeBeddingSet = Number(value) - 1;
+            $('#price').text(bedding_set[set].price_set[typeBeddingSet]);
+            $('#set_size_1').html(bedding_set[set].size_1[typeBeddingSet]);
+            $('#set_size_2').html(bedding_set[set].size_2[typeBeddingSet]);
+        });
+    */
+    $(check_opt).on('change', () => {
+        const value = $('input[name="option"]:checked').val();
+        const typeBeddingSet = 'price_' + value;
+        $('#price').text(bedding_set[set].price_set[typeBeddingSet]);
+        $('#set_size_1').html(bedding_set[set].size_1[typeBeddingSet]);
+        $('#set_size_2').html(bedding_set[set].size_2[typeBeddingSet]);
+    });
+
 
     $('#foto_2').on('click', () => {
         $('#foto_1').css('opacity', 0.4);
